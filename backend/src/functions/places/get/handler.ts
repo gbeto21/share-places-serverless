bf58f83850c6bbda1dbe3d3cefdd66007ce07f2f
Alberto González <albertoglezb@hotmail.com>
2021-11-14 14:00:34 -0600

    The get places functionality was implemented.

--- backend/src/functions/places/get/handler.ts
+++ backend/src/functions/places/get/handler.ts
@@ -1,12 +1,27 @@
 import 'source-map-support/register';
 import { middyfy } from '@libs/lambda';
+import { getPlaces } from "../../../businessLogic/places";
+import { createLogger } from "../../../utils/logger";
 
-const getPlaces: any = async (event) => {
-  const places = []
-  return {
-    statusCode: 201,
-    body: JSON.stringify({ places })
+const logger = createLogger('Handler logger')
+
+const handler: any = async (event) => {
+
+  try {
+    const places = getPlaces('userId')
+ 
+    return {
+      statusCode: 201,
+      body: JSON.stringify({ places })
+    }
+
+  } catch (error) {
+    logger.error(error)
+    return {
+      statusCode: 500,
+      message: "Internal server error."
+    }
   }
 }
 
-export const main = middyfy(getPlaces);
+export const main = middyfy(handler);

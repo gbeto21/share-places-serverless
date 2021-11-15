46cc98fd28e18ab8e3ab18c3bba9e3020099b03d
Alberto González <albertoglezb@hotmail.com>
2021-11-15 11:08:49 -0600

    Files for the auth functionality were added.

---
+++ backend/src/utils/auth/auth0Authorizer.ts
@@ -0,0 +1,91 @@
+import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
+import 'source-map-support/register'
+
+import { verify, decode } from 'jsonwebtoken'
+import { createLogger } from '../logger'
+import Axios from 'axios'
+import { Jwt } from './Jwt'
+import { JwtPayload } from './JwtPayload'
+
+const logger = createLogger('auth')
+
+const jwksUrl = 'https://dev-07sod58o.us.auth0.com/.well-known/jwks.json'
+
+export const handler = async (
+  event: CustomAuthorizerEvent
+): Promise<CustomAuthorizerResult> => {
+  logger.info('Authorizing a user', event.authorizationToken)
+  try {
+    const jwtToken = await verifyToken(event.authorizationToken)
+    logger.info('User was authorized', jwtToken)
+
+    return {
+      principalId: jwtToken.sub,
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: 'Allow',
+            Resource: '*'
+          }
+        ]
+      }
+    }
+  } catch (e) {
+    logger.error('User not authorized', { error: e.message })
+
+    return {
+      principalId: 'user',
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: 'Deny',
+            Resource: '*'
+          }
+        ]
+      }
+    }
+  }
+}
+
+async function verifyToken(authHeader: string): Promise<JwtPayload> {
+  const token = getToken(authHeader)
+  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+
+  const jwtKid = jwt.header.kid
+  let cert: string | Buffer
+
+  try {
+    const jwks = await Axios.get(jwksUrl)
+    const signing = jwks.data.keys.filter((k) => k.kid === jwtKid)[0]
+
+    logger.info('signInKey', signing)
+
+    if (!signing) {
+      throw new Error(`Unable to find a signing key that matches '${jwtKid}'`)
+    }
+    // extract the x509 certificate chain
+    const { x5c } = signing
+
+    cert = `-----BEGIN CERTIFICATE-----\n${x5c[0]}\n-----END CERTIFICATE-----`
+  } catch (error) {
+    logger.error('Error While getting Certificate : ', error)
+  }
+
+  return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
+}
+
+function getToken(authHeader: string): string {
+  if (!authHeader) throw new Error('No authentication header')
+
+  if (!authHeader.toLowerCase().startsWith('bearer '))
+    throw new Error('Invalid authentication header')
+
+  const split = authHeader.split(' ')
+  const token = split[1]
+
+  return token
+}

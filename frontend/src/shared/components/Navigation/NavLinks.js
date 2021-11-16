c9358a00d0892fd2d7009f1f6f1cdceba6911c84
Alberto González <albertoglezb@hotmail.com>
2021-11-15 20:14:08 -0600

    Clean code was implemented in the nav bar.

--- frontend/src/shared/components/Navigation/NavLinks.js
+++ frontend/src/shared/components/Navigation/NavLinks.js
@@ -9,13 +9,6 @@ const NavLinks = props => {
     const auth = useContext(AuthContext)
 
     return <ul className="nav-links">
-        <li>
-            <NavLink to="/" exact>ALL USERS</NavLink>
-        </li>
-        {auth.isLoggedIn &&
-            (<li>
-                <NavLink to={`/${auth.userId}/places`}>MY PLACES</NavLink>
-            </li>)}
         {auth.isLoggedIn && (<li>
             <NavLink to="/places/new">ADD PLACE</NavLink>
         </li>)}

dbc126b57b0ba1e57dd933b9e3cd88dac9471732
Alberto González <albertoglezb@hotmail.com>
2021-11-15 20:07:00 -0600

    The login page was rendericed in the home page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,23 @@
 import React, { Suspense } from 'react';
 import { useAuth } from './shared/hooks/auth-hooks'
 import { AuthContext } from "./shared/context/auth-context";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainNavigation from './shared/components/Navigation/MainNavigation'
+import LoadingSpinner from './shared/components/UIElements/LoadingSpinner';
 
 // const NewPlace = React.lazy(() => import('./places/pages/NewPlace'))
 // const UpdatePlace = React.lazy(() => import('./places/pages/UpdatePlace'))
 // const UserPlaces = React.lazy(() => import('./places/pages/UserPlaces'))
+const Auth = React.lazy(() => import('./user/pages/Auth'))
 
 function App() {
 
   const { token, login, logout } = useAuth()
   let routes = (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainNavigation />} exact />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Auth />} exact />
+      <Route path="/auth" element={<Auth />} exact />
+    </Routes>
   )
 
   return (
@@ -27,10 +28,18 @@ function App() {
         login: login,
         logout: logout
       }}>
-      {/* <MainNavigation /> */}
-      <main>
-        {routes}
-      </main>
+      <BrowserRouter>
+        <MainNavigation />
+        <main>
+          <Suspense fallback={
+            <div className="center">
+              <LoadingSpinner />
+            </div>
+          }>
+            {routes}
+          </Suspense>
+        </main>
+      </BrowserRouter>
     </AuthContext.Provider>
   );
 }
